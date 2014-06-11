500a5a6eb4c845880853921d76c6fc5032c45e1c
Herd <ssherds@gmail.com>
2014-06-11 04:52:09 -0600

    Created the MetricScore class and stubbed out all of its methods

---
+++ BBKeys_Metrics/src/MetricScore.java
@@ -0,0 +1,47 @@
+
+public class MetricScore {
+	private Metric metric;
+	private double value;
+	
+	/**
+	 * Constructor (initializes private variables)
+	 */
+	public MetricScore() {
+		metric = null;
+		value = 0.0;
+	}
+	
+	/**
+	 * Set the value of the Metric
+	 * @param metric - new Metric value of the MetricScore
+	 * @return void
+	 */
+	public void setMetric(Metric metric) {
+		this.metric = metric;
+	}
+	
+	/**
+	 * Get the Metric of the MetricScore
+	 * @return Metric
+	 */
+	public Metric getMetric() {
+		return metric;
+	}
+	
+	/**
+	 * Set the value of the MetricScore
+	 * @param value - new value for the MetricScore
+	 * @return void
+	 */
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	/**
+	 * Get the value of the MetricScore
+	 * @return double
+	 */
+	public double getValue() {
+		return value;
+	}
+}
