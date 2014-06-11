87409f9a60bf08a12d657dfa92f2cb476dfcd4c2
Herd <ssherds@gmail.com>
2014-06-11 06:14:24 -0600

    Created the Employee class and stubbed out all of its methods

---
+++ BBKeys_Metrics/src/Employee.java
@@ -0,0 +1,28 @@
+
+public class Employee extends GradableItem{
+	private byte[] picture;
+	
+	/**
+	 * Constructor (initializes the private variables)
+	 */
+	public Employee() {
+		picture = null;
+	}
+	
+	/**
+	 * Set the value of the picture for the Employee 
+	 * @param picture - the new value of the picture
+	 * @return void
+	 */
+	public void setPicture(byte[] picture) {
+		this.picture = picture;
+	}
+	
+	/**
+	 * Get the value of the picture for the Employee
+	 * @return byte[]
+	 */
+	public byte[] getPicture() {
+		return picture;
+	}
+}
