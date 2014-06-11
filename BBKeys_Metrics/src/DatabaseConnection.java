5c23eafd367a1a63a1fff175e6d7c8a3a747a06e
Herd <ssherds@gmail.com>
2014-06-11 03:55:53 -0600

    Created DatabaseConnection Class and filled in methods

---
+++ BBKeys_Metrics/src/DatabaseConnection.java
@@ -0,0 +1,100 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 
+ * @author Shane
+ *
+ */
+public class DatabaseConnection {
+	
+	private String host;
+	private String port;
+	private String database;
+	private String username;
+	private String password;
+	
+	private Connection con = null;
+	private String connectionURL = null;
+	private ResultSet r = null;
+	ResultSetMetaData rsmd = null;
+	
+	/**
+	 * Constructor which sets the private variables equal to the parameters passed 
+	 * @param host = computer host name or IP address
+	 * @param port = port that the database is being hosted from
+	 * @param database = name of database
+	 * @param username = user accessing the database
+	 * @param password = user's password
+	 */
+	public DatabaseConnection(String host, String port, String database, String username, String password) throws SQLException {
+		this.host = host;
+		this.port = port;
+		this.database = database;
+		this.username = username;
+		this.password = password;
+		
+		setUpConnection();
+	}
+	
+	/**
+	 * Sets up the database connection
+	 */
+	private void setUpConnection() throws SQLException {
+		//check to see if the required driver is installed
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			connectionURL = "jdbc:sqlserver://" + host + ":" + port + ";databaseName=" + database + ";user=" + username + ";password=" + password;
+			con = DriverManager.getConnection(connectionURL);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Driver not installed");
+		} 
+	}
+	
+	/**
+	 * Execute the query 
+	 * @param query = SQL Query
+	 */
+	public void executeQuery(String query) throws SQLException {
+		//try {
+			Statement s = con.createStatement();
+			r = s.executeQuery(query);
+	}
+	
+	/**
+	 * Execute the query and then display the data from the resulting record set
+	 * @param query = SQL Query
+	 */
+	public void executeQueryAndDisplayResults(String query) throws SQLException {
+		Statement s = con.createStatement();
+		r = s.executeQuery(query);
+		
+		//get the number of columns
+		rsmd = r.getMetaData();
+		int columnsNumber = rsmd.getColumnCount();
+		
+		//column number starts at 1
+		//iterate through each row in the record set
+		while (r.next()) {
+			//print each column for the current row
+			for (int i = 1; i < columnsNumber+1; i++) {
+				System.out.print(r.getString(i) + " ");
+			}
+			
+			//print new line
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * Returns the result set from the previous query
+	 */
+	public ResultSet getResultSet() {
+		return r;
+	}
+}
