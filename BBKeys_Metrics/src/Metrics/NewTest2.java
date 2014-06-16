968a52bdce30bf87e2cf4b8d9c113cd6c24f6233
Herd <ssherds@gmail.com>
2014-06-16 13:22:05 -0600

    Created Scheme to run multiple tests

--- BBKeys_Metrics/src/Metrics/NewTest2.java
+++ BBKeys_Metrics/src/Metrics/NewTest2.java
@@ -3,20 +3,12 @@ package Metrics;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NewTest {
+public class NewTest2 {
   @Test
   public void f() {
 	  GradableItem gi = new GradableItem();
 	  gi.setName("Shane");
 	  
 	  Assert.assertEquals(gi.getName(), "Shane");
-  }
-  
-  @Test
-  public void f2() {
-	  GradableItem gi = new GradableItem();
-	  gi.setName("Shane");
-	  
-	  Assert.assertEquals(gi.getName(), "Fail");
   }
 }
