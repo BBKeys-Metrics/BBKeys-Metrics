5ef4f4f84cf70fb3a157b6190bb1af4385e4ae76
Herd <ssherds@gmail.com>
2014-06-18 15:29:05 -0600

    updated register

--- BBKeys_Metrics/src/Metrics/Register.java
+++ BBKeys_Metrics/src/Metrics/Register.java
@@ -1,5 +1,6 @@
 package Metrics;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javafx.application.Application;
@@ -9,10 +10,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -95,12 +98,13 @@ public class Register extends Application {
         
         //Allow elements to grow as the window is resized
         //grow horizontally
+        /*
         ColumnConstraints column0 = new ColumnConstraints();
-        column0.setPercentWidth(20);
+        column0.setPercentWidth(30);
         ColumnConstraints column1 = new ColumnConstraints();
-        column1.setPercentWidth(20);
+        column1.setPercentWidth(30);
         ColumnConstraints column2 = new ColumnConstraints();
-        column2.setPercentWidth(60);
+        column2.setPercentWidth(40);
         ColumnConstraints column3 = new ColumnConstraints();
         column3.setPercentWidth(0);
         ColumnConstraints column4 = new ColumnConstraints();
@@ -111,57 +115,192 @@ public class Register extends Application {
         RowConstraints rowConstraints0 = new RowConstraints();
         rowConstraints0.setPercentHeight(10);
         RowConstraints rowConstraints1 = new RowConstraints();
-        rowConstraints1.setPercentHeight(5);
+        rowConstraints1.setPercentHeight(10);
         RowConstraints rowConstraints2 = new RowConstraints();
         rowConstraints2.setPercentHeight(10);
         RowConstraints rowConstraints3 = new RowConstraints();
-        rowConstraints3.setPercentHeight(65);
+        rowConstraints3.setPercentHeight(10);
         RowConstraints rowConstraints4 = new RowConstraints();
-        rowConstraints4.setPercentHeight(10);
+        rowConstraints4.setPercentHeight(60);
         grid.getRowConstraints().addAll(rowConstraints0, rowConstraints1, rowConstraints2, rowConstraints3, rowConstraints4);
-        
+        */
         //set the minimum width and height of the window
         primaryStage.setMinWidth(500);
         primaryStage.setMinHeight(500);
         
+        Text userIDLabel = new Text("User ID:");
+        userIDLabel.setId("userid");
+        grid.add(userIDLabel, 0, 0, 1, 1);
+        
+        TextField userID = new TextField();
+        userID.setPrefWidth(300);
+        grid.add(userID, 1, 0, 4, 1);
+        
         //create text view which will prompt the user for a username
         Text usernameLabel = new Text("Username:");
         usernameLabel.setId("username"); //used with css
-        grid.add(usernameLabel, 0, 0, 1, 1);   //column, row, num columns spanned, num rows spanned
+        grid.add(usernameLabel, 0, 1, 1, 1);   //column, row, num columns spanned, num rows spanned
         
         //create the text field where the user will enter the username
         TextField username = new TextField();
         username.setPrefWidth(300);
-        grid.add(username, 1, 0, 4, 1);
+        grid.add(username, 1, 1, 4, 1);
         
         //create the password text view
         Text passwordLabel = new Text("Password:");
         passwordLabel.setId("password");
-        grid.add(passwordLabel, 0, 1, 1, 1);
+        grid.add(passwordLabel, 0, 2, 1, 1);
         
         //create the password text field
-        TextField password = new TextField();
+        PasswordField password = new PasswordField();
         password.setPrefWidth(300);
-        grid.add(password, 1, 1, 4, 1);
+        grid.add(password, 1, 2, 4, 1);
+        
+      //create the password text view
+        Text confirmPasswordLabel = new Text("Confirm Password:");
+        confirmPasswordLabel.setId("confirm password");
+        grid.add(confirmPasswordLabel, 0, 3, 1, 1);
+        
+        //create the password text field
+        PasswordField confirmPassword = new PasswordField();
+        confirmPassword.setPrefWidth(300);
+        grid.add(confirmPassword, 1, 3, 4, 1);
         
         //
+        userID.setOnAction((event) -> {       
+        	username.requestFocus();
+        });
+        
         username.setOnAction((event) -> {       
         	password.requestFocus();
         });
         
+        password.setOnAction((event) -> {       
+        	confirmPassword.requestFocus();
+        });
+        
         Button btnRegister = new Button("Commit");
         btnRegister.setAlignment(Pos.TOP_CENTER);
-        grid.add(btnRegister, 0, 2, 2, 1);
+        grid.add(btnRegister, 0, 4, 2, 1);
+        
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 5);
+        actiontarget.setId("actiontarget");
         
         btnRegister.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-            	//create new application and open in new window
-            	Metrics metric = new Metrics();
-                Stage stage = new Stage();
-                //metric.start(stage); //open in new window
-                metric.start(primaryStage);  //open in same window
+            	boolean fieldsBlank = true;
+            	boolean passwordsMatch = false;
+            	boolean duplicateID = true;
+            	boolean validID = false;
+            	boolean duplicateUsername = true;
+            	ResultSet r = null;
+            	DatabaseConnection dbCon = null;
+            	
+            	if (userID.getText().equals("") || username.getText().equals("") || password.getText().equals("") || confirmPassword.getText().equals("")) {
+            		actiontarget.setFill(Color.FIREBRICK);
+                    actiontarget.setText("All fields are required");
+            	}
+            	else {
+            		fieldsBlank = false;
+            	}
+            	
+            	if (!fieldsBlank) {
+            		//compare first password field with second password field
+                	String firstPassword = password.getText();
+                	String secondPassword = confirmPassword.getText();
+                	
+                	if (!firstPassword.equals(secondPassword)) { //passwords don't match
+                		//display error message "Passwords don't match"
+                		actiontarget.setFill(Color.FIREBRICK);
+                        actiontarget.setText("Passwords don't match");
+                	}
+                	else {
+                		passwordsMatch = true;
+                	}
+            	}
+                	
+            	if (passwordsMatch) {
+            		//check for duplicate id in database 
+            		try {
+            			user = new User();
+            			user.setUsername("MetricsApp");
+            			user.setPassword("javafx");
+            			dbCon = new DatabaseConnection("SHANE-PC", "1433", "Metrics", user);	
+            			r = dbCon.executeQuery("SELECT COUNT(employeeID) FROM Metrics.dbo.Users where employeeID = '" + userID.getText() + "'");
+            			r.next();
+            			
+            			if (!r.getString(1).equals("0")) { //duplicate id found
+            				actiontarget.setFill(Color.FIREBRICK);
+                            actiontarget.setText("Duplicate ID Found");
+            			}
+            			else {
+            				duplicateID = false;
+            			}
+            		} catch (SQLException e1) {
+            			e1.printStackTrace();
+            		}
+            	}
+
+            	//check for valid id (i.e. id exists in people)
+            	if (!duplicateID) {
+            		try {
+						r = dbCon.executeQuery("");
+	        			r.next();
+	            		if (!r.getString(1).equals("1")) {
+	            			//display error message "Invalid ID"
+	            		}
+	            		else {
+	            			validID = true;
+	            		}
+            		} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+            	}
+            	
+            	if (validID) {
+            		//check database for duplicate username	
+        			try {
+						r = dbCon.executeQuery("SELECT COUNT(employeeID) FROM Metrics.dbo.Users where username = '" + username.getText() + "'");
+	        			r.next();
+	        			if (!r.getString(1).equals("0")) {
+	        				actiontarget.setFill(Color.FIREBRICK);
+	                        actiontarget.setText("Duplicate Username Found");
+	        			}
+	        			else {
+	        				duplicateUsername = false;
+	        			}
+        			} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+            	}
+            	
+            	if (!duplicateUsername){
+            		String id = userID.getText();
+    				String name = username.getText();
+    				String pass = password.getText();
+    				String encryptedPassword = "";
+    				
+    				//add userID, username, password to the users table in the database
+    				try {
+						dbCon.executeUpdate("INSERT INTO Metrics.dbo.Users (employeeID, username, password) VALUES (" + id + ", '" + name + "', '" + pass + "')");
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+    				
+    				//encrypt password
+    				
+    				//redirect to user home page
+    				Metrics metric = new Metrics();
+                    metric.start(primaryStage);  //open in same window
+            	}        	
             }
+        });
+        
+        confirmPassword.setOnAction((event) -> {       
+        	btnRegister.fire(); //perform commit button click
         });
                         
         //set the size of the window
@@ -179,19 +318,10 @@ public class Register extends Application {
         primaryStage.show();
         
         //start out with the text field having the focus
-        username.requestFocus();
+        userID.requestFocus();
         
         
-        DatabaseConnection dbCon;
-		try {
-			user = new User();
-			user.setUsername("sa");
-			user.setPassword("SQL2k8#1");
-			dbCon = new DatabaseConnection("SHANE-PC", "1433", "Training", user);	
-			dbCon.executeQueryAndDisplayResults("SELECT * FROM Training.dbo.Departments");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+        
 	}
     
     /**
