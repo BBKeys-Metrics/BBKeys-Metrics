7579a98ccadbbbe922488210687f842cb41c705e
rmstock <rickstockham@gmail.com>
2014-06-20 12:26:46 -0600

    Update ScoresScene template
    Corrected some of the display for scores scene.

--- BBKeys_Metrics/src/Metrics/ScoresScene.java
+++ BBKeys_Metrics/src/Metrics/ScoresScene.java
@@ -1,31 +1,35 @@
 package Metrics;
 
 import javafx.application.Platform;
+
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 
 public class ScoresScene {
+	GridPane grid = new GridPane();
+
+	
 	public ScoresScene() {
-		
+		 
 	}
 	
-	public void setScene() {
+	public Scene getScene() {
 		Thread t1 = new Thread(new Runnable() {
 			public void run() {
 				Controller controller = Controller.getInstance();
 				User user = controller.getUser();
 				Preferences pref = controller.getPrefs(user);
-				GridPane grid = new GridPane();
-				Scene scene = new Scene(grid, 300,200);
+				//grid.add(pref.view());
 				Platform.runLater(new Runnable() {
-					@Override
 					public void run() {
-						Metrics.getInstance().getStage().setScene(scene);
+						Metrics.getInstance().getStage().show();
 					}
 				});
 			}
 		});
 		t1.start();
+		Scene scene = new Scene(grid, 300, 200);
+		return scene;
 	}
 
 }
