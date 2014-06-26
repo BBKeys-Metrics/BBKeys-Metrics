3424f0c51d987f2353a2f9ab6b4f0f210438df5a
Herd <ssherds@gmail.com>
2014-06-26 13:02:24 -0600

    Created an EmployeePic class which get the employee picture from the database

---
+++ BBKeys_Metrics/src/Metrics/EmployeePic.java
@@ -0,0 +1,28 @@
+package Metrics;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class EmployeePic {
+	public void getImageData(Connection conn) {
+         byte[] fileBytes;
+         String query;
+         try {
+             query = "SELECT DocData FROM EmployeePics WHERE EmployeeID = '1'";
+             Statement state = conn.createStatement();
+             ResultSet rs = state.executeQuery(query);
+             if (rs.next()) {
+                  fileBytes = rs.getBytes(1);
+                  OutputStream targetFile = new FileOutputStream("C:\\Users\\Shane\\Desktop\\New folder\\new.JPG");
+                  targetFile.write(fileBytes);
+                  targetFile.close();
+            }     
+         }
+         catch (Exception e) {
+             e.printStackTrace();
+         }
+    }
+}
