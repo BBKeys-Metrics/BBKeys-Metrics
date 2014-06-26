2a04e6f0c2a8962b89883334541766abafc4e82f
Herd <ssherds@gmail.com>
2014-06-26 13:56:50 -0600

    Added comments to EmployeePic class

--- BBKeys_Metrics/src/Metrics/EmployeePic.java
+++ BBKeys_Metrics/src/Metrics/EmployeePic.java
@@ -7,19 +7,38 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class EmployeePic {
+	
+	/**
+	 * This method reads a varbinary item from a database and writes that as a temporary image file
+	 * @param conn = database connection
+	 * @param employeeID = employee id for the image desired
+	 */
 	public void getImageData(Connection conn, String employeeID) {
          byte[] fileBytes = null;
          String query;
-         
          try {
+        	 //set up the query to get the varbinary data
              query = "SELECT DocData FROM EmployeePics WHERE EmployeeID = '" + employeeID + "'";
+             
+             //create query
              Statement state = conn.createStatement();
+             
+             //execute query
              ResultSet rs = state.executeQuery(query);
+             
+             //if multiple images are returned, cycle through all of them
              if (rs.next()) {
-                  fileBytes = rs.getBytes(1);
-                  OutputStream targetFile = new FileOutputStream("temp.JPG");
-                  targetFile.write(fileBytes);
-                  targetFile.close();
+            	 //get current image
+                 fileBytes = rs.getBytes(1);
+                 
+                 //create new file
+                 OutputStream targetFile = new FileOutputStream("temp.JPG");
+                 
+                 //write the new file
+                 targetFile.write(fileBytes);
+                 
+                 //close the new file
+                 targetFile.close();
             }     
          }
          catch (Exception e) {
