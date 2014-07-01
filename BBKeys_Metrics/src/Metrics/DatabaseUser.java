2cb6995f224d32b687169b7ee63d79b5b05d544f
Herd <ssherds@gmail.com>
2014-07-01 15:27:32 -0600

    Created database user to seperate confusion between login user and database user

---
+++ BBKeys_Metrics/src/Metrics/DatabaseUser.java
@@ -0,0 +1,62 @@
+package Metrics;
+
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class DatabaseUser{
+	private String username;
+	private String password;
+	
+	/**
+	 * Constructor (initialize private variables)
+	 */
+	public DatabaseUser() {
+		Properties properties = new Properties();
+		try {
+			//fill properties with the data from the file
+			properties.load(User.class.getResourceAsStream("../user.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//set data equal the value of the properties
+		username = properties.getProperty("username");
+		password = properties.getProperty("password");
+	}
+	
+	/**
+	 * Set the value of the username of the User
+	 * @param username - the new value of the username
+	 * @return void
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	/**
+	 * Get the username of the User
+	 * @return String
+	 */
+	public String getUsername() {
+		return username;
+	}
+	
+	/**
+	 * Set the password of the User
+	 * @param password - new value of the password
+	 * @return void
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/**
+	 * Get the password of the User
+	 * @return String
+	 */
+	public String getPassword() {
+		return password;
+	}
+}
+
