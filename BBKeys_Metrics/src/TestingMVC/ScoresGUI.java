60d06e5974329525bdef8a73bbc1d2dacb42e704
Herd <ssherds@gmail.com>
2014-07-07 15:05:11 -0600

    cleaning up some code

--- BBKeys_Metrics/src/TestingMVC/ScoresGUI.java
+++ BBKeys_Metrics/src/TestingMVC/ScoresGUI.java
@@ -3,12 +3,10 @@ package TestingMVC;
 import java.util.Set;
 
 import Metrics.Employee;
-import Metrics.EmployeePic;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
@@ -44,7 +42,6 @@ public class ScoresGUI {
 		});
 		t1.start();
 		grid.add(new Button("Press Me"), 1, 0);
-		//Scene scene = new Scene(grid, 300, 200);
 		Scene scene = new Scene(grid);
 		
 		//get the image of the current user
