af572e066e720d1e4839d16b361d19c7a134c960
Herd <ssherds@gmail.com>
2014-07-16 12:14:50 -0600

    tests

--- BBKeys_Metrics/src/TestingMVC/ResultSetBuilderTest.java
+++ BBKeys_Metrics/src/TestingMVC/ResultSetBuilderTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import Metrics.DatabaseConnection;
 import Metrics.Employee;
 import Metrics.EmployeePic;
+import Metrics.Metric;
 
 public class ResultSetBuilderTest {
 
@@ -25,12 +26,27 @@ public class ResultSetBuilderTest {
 
   @Test
   public void buildID() {
-    throw new RuntimeException("Test not implemented");
+	ResultSet r = DatabaseConnection.getInstance().executeQuery("Select EmployeeID FROM Metrics.dbo.Users WHERE username = 'sherd'");
+	String id = ResultSetBuilder.buildID(r);
+	
+	Assert.assertEquals(id, "1");
   }
 
   @Test
   public void buildMetric() {
-    throw new RuntimeException("Test not implemented");
+	  ResultSet r = DatabaseConnection.getInstance().executeQuery("Select * FROM Metrics.dbo.Metrics WHERE id = '2'");
+	  Metric m =  ResultSetBuilder.buildMetric(r);
+	  int id = m.getID();
+	  String name = m.getName();
+	  int precision = m.getPrecision();
+	  String sortType = m.getSortType();
+	  double weight = m.getWeight();
+	  
+	  Assert.assertEquals(id, 2);
+	  Assert.assertEquals(name, "CreditsCompleted");
+	  Assert.assertEquals(precision, 0);
+	  Assert.assertEquals(sortType, "4");
+	  Assert.assertEquals(weight, 1);
   }
 
   @Test
