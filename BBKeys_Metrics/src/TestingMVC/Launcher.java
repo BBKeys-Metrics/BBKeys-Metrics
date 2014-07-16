e99d07d45108ba04f3c17b4e72b801efdcd74163
Herd <ssherds@gmail.com>
2014-07-16 14:38:12 -0600

    Launcher Javadoc

--- BBKeys_Metrics/src/TestingMVC/Launcher.java
+++ BBKeys_Metrics/src/TestingMVC/Launcher.java
@@ -4,14 +4,26 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
+	
+	/**
+	 * default constructor
+	 */
 	public Launcher() {
 		
 	};
 	
+	/**
+	 * Main class (this is ran when the program is started)
+	 * Calls the launch / start method
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	/**
+	 * runs the initialize method of the Controller class
+	 */
 	@Override
 	public void start(Stage arg0) {
 		Controller.getInstance().initialize(arg0);
